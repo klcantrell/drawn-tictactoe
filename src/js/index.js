33ed909829611d8cddc096bd0cb2f2795c721d1c
Kalalau <cantrellkalalau@gmail.com>
2018-02-28 11:01:25 -0500

    replaced gsap dependency with kute.js

--- src/js/index.js
+++ src/js/index.js
@@ -40,7 +40,7 @@ if (typeof Object.assign != 'function') {
     configurable: true
   });
 }
-//
+// polyfills for IE end
 
 const gameModel = {
   grid: [
@@ -674,16 +674,14 @@ const gameView = {
     }
   },
   drawBoardGrid: function() {
-    let tl = new TimelineMax();
-      tl
-        .from('#vertical1', 0.5, {drawSVG: 0})
-        .from('#vertical2', 0.5, {drawSVG: 0})
-        .from('#horizontal1', 0.5, {drawSVG: 0})
-        .from('#horizontal2', 0.5, {drawSVG: 0});
+    KUTE.fromTo('#vertical1', {draw: '0% 0%'}, {draw: '0% 100%'}, {duration: 500, delay: 200}).start();
+    KUTE.fromTo('#vertical2', {draw: '0% 0%'}, {draw: '0% 100%'}, {duration: 500, delay: 400}).start();
+    KUTE.fromTo('#horizontal1', {draw: '0% 0%'}, {draw: '0% 100%'}, {duration: 500, delay: 600}).start();
+    KUTE.fromTo('#horizontal2', {draw: '0% 0%'}, {draw: '0% 100%'}, {duration: 500, delay: 800}).start();
     for (let i = 0; i < this.squares.length; i++) {
       let square = this.squares[i];
-      let startTime = 0.2 * i;
-      tl.fromTo(square.firstChild, 1, {opacity: 0}, {opacity: 1}, startTime);
+      let startTime = 200 * i;
+      KUTE.to(square.firstChild, {opacity: 1}, {duration: 1000, delay: startTime}).start();
     }
     this.loadBoard();
   },
@@ -715,26 +713,22 @@ const gameView = {
   },
   insertShape: function(id, shape) {
     if (shape === 'X') {
-      return  `<svg class="svg-X" width="75" height="75" viewBox="0 0 75 75">
+      return  `<svg class="svg-X" style="stroke-dasharray: 110; stroke-dashoffset: 110;" width="75" height="75" viewBox="0 0 75 75">
                 <path id="diag_right${id}" d="m 0 0 75 75" stroke="hsla(9, 100%, 76%, 1)" stroke-width="2.5"/>
                 <path id="diag_left${id}" d="m 75 0 -75 75" stroke="hsla(9, 100%, 76%, 1)" stroke-width="2.5"/>
               </svg>`;
     } else {
-      return  `<svg class="svg-O" width="90" height="90" viewBox="3 0 21 21">
+      return  `<svg class="svg-O" style="stroke-dasharray: 110; stroke-dashoffset: 110;" width="90" height="90" viewBox="3 0 21 21">
                   <path id="circle${id}" d="m 13.455845,1.0098512 c -5.4054596,0.019625 -9.7717564,4.3583082 -9.7523971,9.6907208 0.019359,5.332414 4.4170437,9.63928 9.8225031,9.619655 5.40546,-0.01962 9.771757,-4.358308 9.752397,-9.690721 -0.01936,-5.3324135 -4.417044,-9.63927948 -9.822503,-9.6196548 z" fill="transparent" stroke="hsla(42, 100%, 76%, 1)" stroke-width="0.65"/>
                </svg>`;
     }
   },
   drawShape: function(id, shape) {
     if (shape === 'X') {
-      let tl = new TimelineMax();
-      tl
-        .from(`#diag_right${id}`, 0.5, {drawSVG: 0})
-        .from(`#diag_left${id}`, 0.5, {drawSVG: 0});
+      KUTE.fromTo(`#diag_right${id}`, {draw: '0% 0%'}, {draw: '0% 100%'}, {duration: 500}).start();
+      KUTE.fromTo(`#diag_left${id}`, {draw: '0% 0%'}, {draw: '0% 100%'}, {duration: 500, delay: 300}).start();
     } else {
-      let tl = new TimelineMax();
-      tl
-        .from(`#circle${id}`, 1.5, {drawSVG: 0})
+      KUTE.fromTo(`#circle${id}`, {draw: '0% 0%'}, {draw: '0% 100%'}, {duration: 1500}).start();
     }
   },
   reset: function() {
